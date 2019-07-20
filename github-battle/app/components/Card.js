cb7f551f18d84d5a0b043de6a6e9a00f50b2e7d1
David Liang <david.liang@epsi.io>
2019-07-19 22:36:41 -0500

    finished adding the theme toggler and the theme consumer context for the application

--- github-battle/app/components/Card.js
+++ github-battle/app/components/Card.js
@@ -1,29 +1,33 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
+import { ThemeConsumer } from '../contexts/theme'
 export default function Card ({ header, subHeader, avatar, href, name, children }) {
     return (
-        <div className='card bg-light'>
-            <h4 className='header-lg center-text'>
-                {header}
-            </h4>
-            <img
-                className='avatar'
-                src={avatar}
-                alt={`Avatar for ${name}`}
-            />
-            {subHeader && (
-                <h4 className='center-text'>
-                    {subHeader}
-                </h4>
+        <ThemeConsumer>
+            {({ theme }) => (
+                <div className='card bg-light'>
+                    <h4 className='header-lg center-text'>
+                        {header}
+                    </h4>
+                    <img
+                        className='avatar'
+                        src={avatar}
+                        alt={`Avatar for ${name}`}
+                    />
+                    {subHeader && (
+                        <h4 className='center-text'>
+                            {subHeader}
+                        </h4>
+                    )}
+                    <h2 className='center-text'>
+                        <a className='link' href={href}>
+                            {name}
+                        </a>
+                    </h2>
+                    {children}
+                </div>
             )}
-            <h2 className='center-text'>
-                <a className='link' href={href}>
-                    {name}
-                </a>
-            </h2>
-            {children}
-        </div>
+        </ThemeConsumer>
     )
 }
 
--- github-battle/app/components/Loading.js
+++ github-battle/app/components/Loading.js
@@ -1,50 +1,52 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-const styles = {
-    content: {
-        fontSize: '35px',
-        postion: 'absolute',
-        left: '0',
-        right: '0',
-        marginTop: '20px',
-        textAlign: 'center',
 
-    }
+const styles = {
+  content: {
+    fontSize: '35px',
+    position: 'absolute',
+    left: '0',
+    right: '0',
+    marginTop: '20px',
+    textAlign: 'center',
+  }
 }
 
 export default class Loading extends React.Component {
-    constructor() {
-        super(props)
+  constructor(props) {
+    super(props)
 
-        this.state = {
-            content: props.text
-        }
+    this.state = {
+      content: props.text
     }
-    componentDidMount () {
-        this.interval = window.setInterval(() => {
-            this.state.content === text + '...'
-                ? this.setState({ content: text })
-                : this.setState(({ content }) => ({ content: content + '.' }))
-        }, speed)
-    }
-    componentWillUnmount () {
-        window.clearInterval(this.interval)
-    }
-    render() {
-        return (
-            <p style={styles.content}>
-                {this.state.content}
-            </p>
-        )
-    }
+  }
+  componentDidMount () {
+    const { speed, text } = this.props
+
+    this.interval = window.setInterval(() => {
+      this.state.content === text + '...'
+        ? this.setState({ content: text })
+        : this.setState(({ content }) => ({ content: content + '.' }))
+    }, speed)
+  }
+  componentWillUnmount () {
+    window.clearInterval(this.interval)
+  }
+  render() {
+    return (
+      <p style={styles.content}>
+        {this.state.content}
+      </p>
+    )
+  }
 }
 
-Loading.PropTypes = {
-    text: PropTypes.string.isRequired,
-    speed: PropTypes.number.isRequired,
+Loading.propTypes = {
+  text: PropTypes.string.isRequired,
+  speed: PropTypes.number.isRequired,
 }
 
 Loading.defaultProps = {
-    text: 'Loading',
-    speed: 300,
+  text: 'Loading',
+  speed: 300
 }
